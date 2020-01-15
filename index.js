c69672f1898a61b0120649e5cde15370a0660070
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-15 17:46:46 -0500

    reimplement map() -test driven implementation

--- index.js
+++ index.js
@@ -8,5 +8,15 @@ module.exports = {
     for (let index in arr) {
       fn(arr[index], index);
     }
+  },
+
+  map(arr, fn) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result.push(fn(arr[i], i));
+    }
+    return result;
   }
+
+  //reimplement map() for testing practice
 };
