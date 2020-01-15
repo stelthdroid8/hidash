d7b75ac0f49eaaf3d6e1400ae44cd3fd546b425b
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-15 17:59:34 -0500

    fix global scoping issue, fix first error stopping all subsequent code, add description to test

--- index.test.js
+++ index.test.js
@@ -1,25 +1,39 @@
 const { forEach, map } = require('./index.js');
-let sum = 0;
-forEach([1, 2, 3], value => {
-  sum += value;
+
+const test = (desc, fn) => {
+  console.log('-----------', desc);
+  try {
+    fn();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// TESTS using test()
+//----foreEeach testing
+test('The forEach function', () => {
+  let sum = 0;
+  forEach([1, 2, 3], value => {
+    sum += value;
+  });
+
+  if (sum !== 6) {
+    throw new Error('expected summing array to be 6');
+  }
 });
+//----map testing
+test('The map function', () => {
+  const result = map([1, 2, 3], value => {
+    return value * 2;
+  });
 
-if (sum !== 6) {
-  throw new Error('expected summing array to be 6');
-}
-
-const result = map([1, 2, 3], value => {
-  return value * 2;
+  if (result[0] !== 2) {
+    throw new Error(`expected to find 2, but found :${result[0]}`);
+  }
+  if (result[1] !== 4) {
+    throw new Error(`expected to find 4, but found :${result[1]}`);
+  }
+  if (result[2] !== 6) {
+    throw new Error(`expected to find 6, but found :${result[2]}`);
+  }
 });
-
-//result SHOULD be [2,4,6]
-
-if (result[0] !== 2) {
-  throw new Error(`expected to find 2, but found :${result[0]}`);
-}
-if (result[1] !== 4) {
-  throw new Error(`expected to find 4, but found :${result[1]}`);
-}
-if (result[2] !== 6) {
-  throw new Error(`expected to find 6, but found :${result[2]}`);
-}
